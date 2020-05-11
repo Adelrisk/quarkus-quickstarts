b29afe55b223d67fd1a1203a535be7c69de86ace
Adelrisk <8825136+Adelrisk@users.noreply.github.com>
2020-05-11 16:56:00 +0200

    Suppress exceptions, allow messaging to continue

--- amqp-quickstart/src/main/java/org/acme/amqp/PriceConverter.java
+++ amqp-quickstart/src/main/java/org/acme/amqp/PriceConverter.java
@@ -6,6 +6,10 @@ import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
 import io.smallrye.reactive.messaging.annotations.Broadcast;
+import io.vertx.core.json.JsonObject;
+import org.eclipse.microprofile.reactive.messaging.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A bean consuming data from the "prices" AMQP queue and applying some conversion.
@@ -14,13 +18,26 @@ import io.smallrye.reactive.messaging.annotations.Broadcast;
 @ApplicationScoped
 public class PriceConverter {
 
+    private static Logger LOG = LoggerFactory.getLogger(PriceConverter.class);
+
     private static final double CONVERSION_RATE = 0.88;
 
     @Incoming("prices")
     @Outgoing("my-data-stream")
     @Broadcast
-    public PriceDouble process(PriceInteger priceInUsd) {
-        return new PriceDouble(priceInUsd.getPrice() * CONVERSION_RATE);
+    public PriceDouble process(Message<JsonObject> priceInUsd) {
+	LOG.warn("Printing the payload: {}", priceInUsd.getPayload());
+	try {
+	    JsonObject payload = priceInUsd.getPayload();
+	    LOG.warn("Printing the class: {}", payload.getClass());
+	    PriceInteger inPrice = payload.mapTo(PriceInteger.class);
+	    return new PriceDouble(inPrice.getPrice() * CONVERSION_RATE);
+
+	} catch(Exception e) {
+	    LOG.error("Error checking the payload class.", e);
+	}
+	// 3 was randomly determined.
+        return new PriceDouble(3 * CONVERSION_RATE);
     }
 
 }
